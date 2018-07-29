3f57abd95f6c92a85c445c9f704d1144b7988cb5
Dennis Löhmann <dennis@macbook-pro-2.home>
2018-07-29 15:23:29 +0200

    Added Assignment for week1

---
+++ src/assignment_1/Car.java
@@ -0,0 +1,20 @@
+package assignment_1;
+
+public class Car {
+	// ...
+
+	public Car(String name, int hp, int vmax) {
+	// add your code here...
+	}
+
+	public String toString() {
+		return "";
+		// add your code here...
+	}
+	
+	public static void main(String[] args) {
+		  Car myCar = new Car("Volkswagen", 115, 200);
+		  System.out.println(myCar.toString());
+		  
+    } 
+}
