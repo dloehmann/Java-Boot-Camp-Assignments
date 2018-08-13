274ab5670683e78f154ad26806bf679d7726ddea
Dennis Löhmann <dennis@macbook-pro-2.lan>
2018-08-13 14:53:28 +0200

    Added Season.java

--- src/soccer/Season.java
+++ src/soccer/Season.java
@@ -1,10 +1,13 @@
 package soccer;
 
+import soccerGame_solution.Team;
+
 public class Season {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Soccer Game");
+		Team chelsea = new Team(1, "Chelsea FC");
+		System.out.println(chelsea.getId() + ": " + chelsea.getName());
 	}
 
 }
